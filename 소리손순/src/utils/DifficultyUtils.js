94fc766e0b47082c92b34a7e79163180dd7d813d
KimTaekgyoon <142713364+KimTaekgyoon@users.noreply.github.com>
2025-01-11 04:59:44 +0900

    Delete 소리손순/src directory

--- 소리손순/src/utils/DifficultyUtils.js
+++
@@ -1,8 +0,0 @@
-export const getDifficultyKor = (difficulty) => {
-    switch (difficulty) {
-        case 'LEVEL_1' : return "쉬움";
-        case 'LEVEL_2' : return "보통";
-        case 'LEVEL_3' : return "어려움";
-        default : return "쉬움";
-    }
-}
